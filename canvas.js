1f2b6a630e18beedc9e541889a6c83c44cf2e4b2
Paul Klinski <Klinskipaul@gmail.com>
2016-11-04 13:42:45 +0100

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>from letter to picture</title>
+  <meta name="description" content="something">
+  <meta name="author" content="me">
+  <link rel = "stylesheet" href = "css/style.css" />
+</head>
+<body>
+  <script type="text/javascript" src="lib/p5.min.js"></script>
+  <script type="text/javascript" src="js/start-letter.js"></script>
+  <script src="//use.edgefonts.net/miama.js"></script>
+  </body>
+  </html>
